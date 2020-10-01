f1f9a6fd74c541eefd0df8f692bfeb81cd5431ce
Danila Kiselev <kiselev.danila.03@mail.ru>
2020-10-01 23:16:30 +0300

    desktop first

--- src/js/page.js
+++ src/js/page.js
@@ -6,158 +6,6 @@ $(function() {
     $("html, body").animate({ scrollTop: dn }, 500);
   });
 });
-// links
-function createOverlay(type) {
-  var mouseOutside;
-  if (type === "callback") {
-    var overlayTemplate = $("#overlayCallbackTemplate").html();
-  }
-  if (type === "analysis") {
-    var overlayTemplate = $("#overlayAnalysisTemplate").html();
-  }
-  if ($(".overlay")) {
-    $("body").append(overlayTemplate);
-    var overlay = $(".overlay");
-    var overlayWindow = $(".overlay__window");
-    overlay.fadeOut(0, function() {
-      overlay.fadeIn(1000);
-    });
-    $(".overlay__input").each((ndx, item) => {
-      $(item).on("keydown", () => {
-        $(item)
-          .siblings(".overlay__input-required")
-          .css({
-            display: "none"
-          });
-      });
-      $(item).on("keyup", () => {
-        if ($(item).val() === "") {
-          $(item)
-            .siblings(".overlay__input-required")
-            .css({
-              display: "block"
-            });
-        }
-      });
-    });
-  }
-  overlayWindow.on("mouseleave", e => {
-    mouseOutside = true;
-  });
-  overlayWindow.on("mouseenter", e => {
-    mouseOutside = false;
-  });
-  overlay.on("click", e => {
-    if (mouseOutside === true) {
-      overlay.fadeOut(500, function() {
-        overlay.remove();
-      });
-    }
-  });
-  $(".overlay__close").on("click", () => {
-    overlay.fadeOut(500, function() {
-      overlay.remove();
-    });
-  });
-}
-$(".btn-callback").on("click", e => {
-  e.preventDefault();
-  createOverlay("callback");
-});
-$(".analysis__link").each((ndx, item) => {
-  $(item).on("click", e => {
-    e.preventDefault();
-    createOverlay("analysis");
-    var analysisNdx = ndx;
-    var itemHref = $(item).attr("href");
-    $(".overlay__label").each((ndx, item) => {
-      if (ndx === analysisNdx) {
-        $(item).click();
-      }
-      $(item).on("click", () => {
-        analysisNdx = ndx;
-        $(".analysis__link").each((ndx, item) => {
-          if (ndx === analysisNdx) {
-            itemHref = $(item).attr("href");
-            $(".overlay__btn").attr("href", itemHref);
-          }
-        });
-      });
-    });
-    $(".overlay__btn").attr("href", itemHref);
-  });
-});
-// overlay
-var howBuyItem;
-$(".how-buy__item").each((ndx, item) => {
-  $(item).on("click", () => {
-    $(".how-buy__item").removeClass("active");
-    howBuyItem = $(item);
-    $(item).addClass("active");
-    $(".how-buy__template").each((ndx, item) => {
-      if (howBuyItem.attr("data-type") === $(item).attr("data-type")) {
-        $(".how-buy__content").html($(item).html());
-      }
-    });
-  });
-});
-// how-buy section
-$(window).scroll(function() {
-  let winScrollTop = $(this).scrollTop();
-  let winHeight = $(window).height();
-  let target;
-  let targetPos;
-  let scrollToElem;
-  $(".container__heading").each((ndx, item) => {
-    target = $(item);
-    targetPos = target.offset().top;
-    scrollToElem = targetPos - winHeight;
-    if (winScrollTop > scrollToElem) {
-      $(item)
-        .find(".container__heading-front")
-        .animate(
-          {
-            left: "6%"
-          },
-          750
-        );
-    }
-  });
-});
-// for heading section
-$(".product__bg-text").each((ndx, item) => {
-  $(item).css({
-    width: `${$(item).width() + 17}px`
-  });
-  let textValue = parseFloat(
-    $(item)
-      .html()
-      .replace(/\s/g, "")
-      .replace(/,/, ".")
-  );
-  var fraction = 0;
-  if (Number.isInteger(textValue) === false) {
-    fraction = 1;
-  }
-  $(item).animate(
-    { num: textValue },
-    {
-      duration: 5000,
-      easing: "linear",
-      step: function(num) {
-        this.innerHTML = ((num + 3).toFixed(fraction) + "")
-          .replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, "$1 ")
-          .replace(".", ",");
-      },
-      complete: function() {
-        $(item).css({
-          width: `auto`
-        });
-      }
-    }
-  );
-});
-// values in about product .replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g,"$1 ");
 const nav = $(".nav");
 $(".header__humburger").on("click", () => {
   if (nav.hasClass("menu")) {
@@ -174,3 +22,51 @@ $(".header__humburger").on("click", () => {
     });
   }
 });
+var sliderItems = $(".slider__item");
+var sliderList = $(".slider__list");
+var sliderWidth = sliderList.width();
+$(window).resize(function() {
+  sliderWidth = sliderList.width();
+});
+var sliderBreadcrumbs = $(".slider__breadcrumbs-item");
+var currentDisplacement = 0;
+var idIntervals = 0;
+sliderList.append(
+  sliderList
+    .children()
+    .first()
+    .clone()
+);
+sliderBreadcrumbs.each((ndx, item) => {
+  $(item).on("click", () => {
+    moveSlide(ndx);
+    clearInterval(idIntervals);
+    idIntervals = setInterval(function() {
+      moveSlide(currentDisplacement + 1);
+    }, 5000);
+  });
+});
+function moveSlide(slidesToMove) {
+  currentDisplacement = slidesToMove;
+  sliderBreadcrumbs.removeClass("active");
+  sliderList.animate(
+    { left: -currentDisplacement * sliderWidth },
+    500,
+    function() {
+      if (currentDisplacement === sliderItems.length) {
+        currentDisplacement = 0;
+        sliderList.css({
+          left: "0px"
+        });
+      }
+      sliderBreadcrumbs.each((ndx, item) => {
+        if (ndx === currentDisplacement) {
+          $(item).addClass("active");
+        }
+      });
+    }
+  );
+}
+idIntervals = setInterval(function() {
+  moveSlide(currentDisplacement + 1);
+}, 5000);
